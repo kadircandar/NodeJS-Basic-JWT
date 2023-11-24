6c58436b98437965b190640617e59dc9674ddcf1
kadircandar <kadir@wellbees.co>
2023-11-24 12:56:20 +0300

    .evn updated for jwt secret

--- app.js
+++ app.js
@@ -24,8 +24,10 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 // Authorization
 app.use(function(req, res, next) {
+
+  console.log("Secret : ", process.env.SECRET)
   if (req.headers && req.headers.authorization && req.headers.authorization.split(' ')[0] === 'JWT') {
-    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], 'RESTFULAPIs', function(err, decode) {
+    jsonwebtoken.verify(req.headers.authorization.split(' ')[1], process.env.SECRET, function(err, decode) {
       if (err) req.user = undefined;
       req.user = decode;
       next();
